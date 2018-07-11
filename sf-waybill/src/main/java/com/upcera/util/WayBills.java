621ceee8680de4b603a9e070104e3f603c6e9490
pandahuangwei@163.com <1qaz2wsx3edc>
2018-07-12 00:09:42 +0800

    add

---
+++ sf-waybill/src/main/java/com/upcera/util/WayBills.java
@@ -0,0 +1,32 @@
+package com.upcera.util;
+
+import com.sf.csim.express.service.CallExpressServiceTools;
+
+public class WayBills {
+
+    private static final String REQ_URL = "https://bsp-oisp.sf-express.com/bsp-oisp/sfexpressService";
+    private static final String CLIENT_CODE = "AECKQJS";//此处替换为您在丰桥平台获取的顾客编码
+    private static final String CHECK_WORD = "fCfDsgQatwYbF8dW9slS2WfRdqGbNAKO";//此处替换为您在丰桥平台获取的校验码
+
+    private static CallExpressServiceTools client = CallExpressServiceTools.getInstance();
+
+
+
+    private static String getWayBillXml(String trackingNumber) {
+       String myReqXML = getReqXml(CLIENT_CODE,trackingNumber);
+      return client.callSfExpressServiceByCSIM(REQ_URL, myReqXML, CLIENT_CODE, CHECK_WORD);
+    }
+
+    private static String getReqXml(String clientCode, String trackingNumber) {
+        return new String("\uFEFF<Request service='RouteService' lang='zh-CN'>\n" +
+                "<Head>" + clientCode + "</Head>\n" +
+                "<Body>\n" +
+                "<RouteRequest\n" +
+                "tracking_type='1'\n" +
+                "method_type='1'\n" +
+                "tracking_number='" + trackingNumber + "'/>\n" +
+                "</Body>\n" +
+                "</Request>");
+    }
+
+}
